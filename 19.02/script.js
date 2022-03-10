d3ebe545b3f83b0c3e17481473ac4966096ac1ab
Mirlan Kasymaliev <k.m.e.151190@gmail.com>
2022-03-10 21:05:27 +0600

    feat: added hw-19.02

---
+++ 19.02/script.js
@@ -0,0 +1,15 @@
+//Классная работа
+let name1 = 'Mirlan';
+alert (name1);
+
+let name2 = 'Kasymaliev Mirlan';
+alert (name2);
+//alert('heys')
+//1)
+let name ='Mirlan!';
+alert('Привет,' + name);
+
+//2)
+let age = '30';
+alert('Мне'+age+'лет!');
+
