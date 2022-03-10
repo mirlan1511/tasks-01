8476ff469d9e197a6355acc75ba7d9803523cf36
Mirlan Kasymaliev <k.m.e.151190@gmail.com>
2022-03-10 21:07:34 +0600

    01.03

---
+++ 01.03/script.js
@@ -0,0 +1,30 @@
+//1)
+/*const userSalariet = {
+    name1: 'Ella',
+    name2: 'Sophia',
+    name3: 'Ellie'
+}
+console.log(userSalariet.name1);
+console.log(userSalariet.name2);*/
+
+//2)
+/*let sum = {
+    a : 5,
+    b : 6,
+    c : 10
+}
+console.log(sum.a + sum.b + sum.c);
+
+
+//3)
+/*let number;
+let data = true;
+if(data === true){
+    number = 3;
+} else{
+    number = 5;
+}
+ console.log(number);*/
+
+let result = (data === true) ? number = 3 : number = 5;
+console.log(result);
