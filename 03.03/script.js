562fd313d7ea6f1b184a225ba4577235c9ae8514
Mirlan Kasymaliev <k.m.e.151190@gmail.com>
2022-03-10 20:54:06 +0600

    feat: added hw 03.03

---
+++ 03.03/script.js
@@ -0,0 +1,24 @@
+
+// 1 task
+let arr = [];
+arr = ['a', 'b,', ' c', 'd'];
+console.log(arr[0],'+',arr[1], arr[2], '+', arr[3]);
+
+//2 tasks
+let numbers = [];
+numbers = [10, 15, 9, 10];
+
+let result1 = []
+result1 = numbers[0] * numbers[1];
+console.log(result1); // 10*15=150
+
+let result2 = []
+result2 = numbers[2] * numbers[3];
+console.log(result2); // 9*10=90
+
+console.log(result1 + result2); // 150 + 90 = 240
+
+// 3 tasks
+let alf = ['a', 'b', 'c'];
+alf.push('1', '2', '3');
+console.log(alf);
--- 05.03/script.js
+++ 05.03/script.js
@@ -36,12 +36,15 @@ console.log(sum);*/
 
 
 // -----Задача №4------
+
 /*let arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+let result = [];
 
 for (let i = 0; i < arr.length; i++) {
     if (arr[i] % 2 == 0) {
         console.log(arr[i]);
     }
+    result.push(arr);
 }*/
 
 
