1d745c65a0eaf854edbfc0a60bd2d86b99e2afb6
Mirlan Kasymaliev <k.m.e.151190@gmail.com>
2022-03-10 21:02:14 +0600

    feat: added hw-22.02

---
+++ 22.02/script.js
@@ -0,0 +1,26 @@
+//1
+let firstNum = 1; // camelCase
+let second_num = 2; // snake_case
+let ThirdNum = 3; // PascalCase
+
+alert (firstNum);
+alert (second_num);
+alert (ThirdNum);
+
+//2
+let str1 = 'Салам ';
+let str2 = 'Бишкек';
+
+alert (str1 + str2);
+
+//3
+let a = 17;
+let b = 10;
+let c = a - b;
+let d = 7;
+let result = a + b;
+
+alert (result);
+
+
+
