be42cf05cec992e79c0446ad4d349930554f0f8c
Mirlan Kasymaliev <k.m.e.151190@gmail.com>
2022-03-10 20:39:14 +0600

    feat: added home work 05.03

---
+++ 05.03/script.js
@@ -0,0 +1,60 @@
+// ------------- Задача №1 ----------
+
+/*let arr = [1,2,3,4,5,6,7,8,9,10];
+for (let i = 0; i <= 10; i++){
+    console.log(arr[i]);
+}*/
+
+
+// Задача №2
+
+/*let summa = 0;
+let arr= [1, 2, 5, 6, 88, 5];
+
+for (let i = 0; i < arr.length; i++){
+    summa += arr[i]
+}
+console.log(summa);*/
+
+
+// -------- Задача №3 -----------
+/*let arr = [1,2,4,6,8,10,12,14,16,18];
+let result = [];
+
+for (let i = 0; i < arr.length; i++){
+    let sqr = arr[i] * arr [i];
+    result.push(sqr);
+}
+console.log(result);
+
+//Суммируем квадраты:
+let sum = 0;
+for (let j = 0; j < result.length; j++){
+    sum += result[j];
+}
+console.log(sum);*/
+
+
+// -----Задача №4------
+/*let arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 == 0) {
+        console.log(arr[i]);
+    }
+}*/
+
+
+// ------Задача №5------
+let arr = [];
+for (i = 0; i < 10; i++){
+    arr[i] = 'X' ;
+}
+console.log(arr);
+
+
+
+
+
+
+
