6ec76a860bf99f601c08a736d1310acd951d043a
Mirlan Kasymaliev <k.m.e.151190@gmail.com>
2022-03-12 20:40:43 +0600

    feat: added task 1,2,3,

--- 10.03/script.js
+++ 10.03/script.js
@@ -1,9 +1,20 @@
-let arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+/*let arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 let result = [];
 
 for (let i = 0; i < arr.length; i++) {
     if (arr[i] % 2 != 0) {
         console.log(arr[i]);
     }
-    result.push(arr[i]);
+    result.push(arr[i]); //вывод не чётных чисел массива
+}*/
+
+
+
+function minNum(a,b) {
+    return (a < b ) ? a : b;
 }
+function showNum(x) {
+    console.log(x);
+}
+let result = minNum(86, 66);
+showNum(result);
