87c80c23a0aee50895c649e453166d15bd363e4f
Mirlan Kasymaliev <k.m.e.151190@gmail.com>
2022-04-12 20:34:34 +0600

    added hw-26.03

---
+++ 02.04./script.js
@@ -0,0 +1,11 @@
+const form = document.querySelector('form');
+
+form.onsubmit = () => {
+    let lastName = document.querySelector ('input[type=text]');
+    let name = document.querySelector ('input[type=text]');
+    let middleName = document.querySelector ('input[type=text]');
+    let birthDate = document.querySelector('input[type=date]');
+    let number = document.querySelector('input[type=number]');
+    }
+
+    console.log(lastName.value);
